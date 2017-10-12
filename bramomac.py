25d103a5ba02ee2a5bab8158576ed065f99b2747
Gia90 <Gia90@users.noreply.github.com>
2017-10-12 23:25:35 +0200

    Added --online option to mac functionality
    Fixed requirements.txt by adding requests dependency

--- bramomac.py
+++ bramomac.py
@@ -1,43 +1,49 @@
-#API base url,you can also use https if you need
-url = "https://macvendors.co/api/"
-
-def vendor_from_api(macaddr):
-	r=requests.get(url+macaddr);
-	return r.json()
-
-
+import requests
 
 # Download oui.txt from: http://standards-oui.ieee.org/oui.txt
 # or run "update-oui" in Kali Linux
 # Brand, Model and other info from MAC address
 class BraMoMac:
-	_ouidb = "/var/lib/ieee-data/oui.txt"
-	
-	def __init__(self, ouidb=None):
+	_local_ouidb = "/var/lib/ieee-data/oui.txt"
+	_online_ouidb = "http://standards-oui.ieee.org/oui.txt"
+	_online = False
+
+	def __init__(self, ouidb=None, online=False):
 		if(ouidb):
-			self._ouidb = ouidb
-	
+			self._local_ouidb = ouidb
+		self._online = online
+
 	def __extract_oui(self, macaddr):
 		oui = macaddr if( len(macaddr) == 8) else macaddr[0:8]
 		oui = oui.replace(":","").upper()
 		return oui
-	
+
+	def __open_ouidb(self):
+		ouidb_content = None
+		if(self._online):
+			try:
+				ouidb_content = requests.get(self._online_ouidb, stream=True).iter_lines(decode_unicode=True, delimiter="\r\n")
+			except requests.exceptions.RequestException as e:
+				raise Exception( "Cannot get \""+self._online_ouidb+"\". Is it up?" )
+		else:
+			try:
+				ouidb_content = open( self._local_ouidb, 'r' )
+			except IOError:
+				raise Exception( "Cannot open \""+self._local_ouidb+"\". Is it correct?" )
+		return ouidb_content
+
 	def get_vendor(self, macaddr):
 		oui = self.__extract_oui(macaddr)
 		vendors = []
-		try:
-			with open( self._ouidb, 'r' ) as ouif:
-				for row in ouif:
-					if oui == row[0:6]:
-						vendor = row
-					elif( "vendor" in locals() ):
-						if row.strip() != "":
-								vendor += row
-						else:
-							vendor = vendor.split("\t\t", 1)[1].strip().replace("\r\n\t\t\t\t", " ") 
-							vendors.append(vendor);
-							del vendor
-		except IOError:
-			raise Exception( "Cannot open \""+self._ouidb+"\". Is it correct?" )
+		ouidb = self.__open_ouidb()
+		for row in ouidb:
+			if oui == row[0:6]:
+				vendor = row.strip()
+			elif( "vendor" in locals() ):
+				if row.strip():
+						vendor += row.strip() + " "
+				else:
+					vendor = vendor.split("\t\t", 1)[1].strip().replace("\r\n\t\t\t\t", " ")
+					vendors.append(vendor);
+					del vendor
 		return vendors
-
